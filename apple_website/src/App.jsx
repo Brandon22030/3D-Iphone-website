ed0c988d99f88d11d0cc1e8c06896504de0f1230
Brandon22030 <daniel.medehou@epitech.eu>
2024-11-18 08:27:37 +0100

    [ADD]: navbar

--- apple_website/src/App.jsx
+++ apple_website/src/App.jsx
@@ -1,13 +1,9 @@
 import Navbar from "./components/Navbar"
-import Highlights from "./components/Highlights"
-import Hero from "./components/Hero"
 
 const  App = () => {
   return (
     <main className="bg-black">
       <Navbar/>
-      <Hero/>
-      <Highlights/>
     </main>
   )
 }
